3ab8084eccb14cf6c4bee889e3e30d714ece5d12
Jesus Cuapio <juanesmas50@gmail.com>
2024-04-27 09:51:27 -0600

    Cambio en el acceso a rutas src en los archivos html debido a cambio en el codigo del server

--- client/src/js/insertar-img-fichas.js
+++ client/src/js/insertar-img-fichas.js
@@ -2,7 +2,6 @@ const botones = document.querySelectorAll('.ficha-button');
 botones.forEach(boton =>{
     const id = boton.id;
     boton.innerHTML=
-    `<img src="../src/assets/img/ficha-roja.png" alt="ficha-roja" class="ficha-roja ficha-roja${id}">
-    
-    <img src="../src/assets/img/ficha-amarilla.png" alt="ficha-amarilla" class="ficha-amarilla ficha-amarilla${id}">`
+    `<img src="../assets/img/ficha-roja.png" alt="ficha-roja" class="ficha-roja ficha-roja${id}">
+    <img src="../assets/img/ficha-amarilla.png" alt="ficha-amarilla" class="ficha-amarilla ficha-amarilla${id}">`
 })
