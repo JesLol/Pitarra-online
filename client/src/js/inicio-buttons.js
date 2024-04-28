1c978ba1ace8f433ee91f6f3a9abf87d93c89635
Jesus Cuapio <juanesmas50@gmail.com>
2024-04-28 14:14:03 -0600

    Se modifican scripts para que solo los usuarios logeados puedan ingresar al online y  se inician las primeras partes de la conexion websocket

--- client/src/js/inicio-buttons.js
+++ client/src/js/inicio-buttons.js
@@ -2,6 +2,36 @@ document.addEventListener("DOMContentLoaded",()=>{
     const infoPitarraBtn = document.getElementById('ver-info-pitarra-btn');
     const jugarOnlineBtn = document.getElementById('jugar-online-btn');
     const jugarOfflineBtn = document.getElementById('jugar-offline-btn');
+    const closeSessionBtn = document.getElementById('close-sesion-btn');
+    // Revisar si el usuario esta registrado
+    const cookies = document.cookie;
+    const cookiesObj = cookies.split(';').reduce((acc, cookie) => {
+        const [name, value] = cookie.trim().split('=');
+        acc[name] = value;
+        return acc;
+    }, {});
+    const miCookie = cookiesObj['jwt'];
+    if(miCookie){
+        document.getElementById('login-btn-li').style.display = "none";
+        document.getElementById('register-btn-li').style.display = "none";
+        document.getElementById('close-sesion-btn-li').style.display = "block";
+        jugarOnlineBtn.addEventListener('click',()=>{
+            window.location.href = "online-game/"
+        });
+        closeSessionBtn.addEventListener('click',()=>{
+            document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+            document.location.href = "/"
+        })
+    }
+    else{
+        jugarOnlineBtn.addEventListener('click',()=>{
+            Swal.fire({
+                icon: "info",
+                text: "Para jugar en linea primero necesitas iniciar sesion.",
+                confirmButtonColor: '#1f5df1'
+            })
+        })
+    }
 
     infoPitarraBtn.addEventListener('click',()=>{
         Swal.fire({
@@ -10,7 +40,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             html: `
                 <section>
                     <article style="display:grid; gap:15px;">
-                        <img src="./src/assets/img/juego-de-la-pitarra.png">
+                        <img src="assets/img/juego-de-la-pitarra.png" style="width:100%;">
                         <div>
                             <h4>Descripcion del juego</h4>
                             <p style="text-align:justify;">Cada jugador cuenta con doce fichas, las cuales se van a ir poniendo alternativamente en el talero, la intención es hacer tercias ya sea de forma horizontal, vertical o diagonal, cuando se forma una tercia se le quita una ficha al oponente, la intención es hacer mayo numero de tercias y que el oponente haga las menos posibles, las fichas rojas son las que empiezan, cuando todos han colocado sus fichas viene la parte dinámica, que es formar tercias moviendo las fichas que ya están en el tablero, solo se pueden mover sobre las líneas, nunca saltando espacios. El ganador se define cuando dejas a tu oponente solo con dos fichas en el tablero, o bien se rinde, dándole la mano de forma caballerosa</p>
@@ -32,9 +62,6 @@ document.addEventListener("DOMContentLoaded",()=>{
             showCancelButton: false,
             confirmButtonText: `¡Listo!`
           });
-    });
-    jugarOnlineBtn.addEventListener('click',()=>{
-        window.location.href = "online-game/"
     });
     jugarOfflineBtn.addEventListener('click',()=>{
         window.location.href = "offline-game/"
